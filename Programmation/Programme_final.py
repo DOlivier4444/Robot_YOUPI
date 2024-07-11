bd54aa2d3a4fcd092c0aa9833acf4c4e5f5c533b
DOlivier4444 <146378066+DOlivier4444@users.noreply.github.com>
2024-07-12 00:48:03 +0200

    Add files via upload

--- Programmation/Programme_final.py
+++ Programmation/Programme_final.py
@@ -3,23 +3,24 @@
 #===============================================================================================#
 #																								#
 #										Robot youpi	- Olivier DANIEL							#
-#										Definition des variables								#
+#																								#
+#										0) DEFINITION DES VARIABLES								#
 #																								#
 #===============================================================================================#
 
 
-# chargement des modules
-import threading
+# 0.1) chargement des modules
+
 import curses
-from gpiozero import LEDBoard # type: ignore (ignorer l'"erreur" dans vscode)
-import pygame # type: ignore
-from pygame.locals import * # type: ignore
+#import threading --> Utilisable plus tard ?
+from gpiozero import LEDBoard                               # type: ignore (ignorer l'"erreur" dans vscode)
+import pygame                                               # type: ignore
+from pygame.locals import *                                 # type: ignore
 from time import sleep
 
 
-# CREATION DES E/S - VARIABLES - CONSTANTES
+# 0.2) Création des E/S - VARIABLES - CONSTANTES
 
-# initialisation des sorties
 LEDBoard("GPIO18") # bit7 = Validation_sens_rotation = D7 
 LEDBoard("GPIO23") # bit6 = Validation_pas_moteur    = D6 
 LEDBoard("GPIO24") # bit5 = moteur6__pince           = D5 = .....,1,0,1
@@ -30,9 +31,14 @@ LEDBoard("GPIO20") # bit1 = moteur2__epaule          = D1 = .....,0,0,1
 LEDBoard("GPIO21") # bit0 = moteur1__base            = D0 = .....,0,0,0
 
 
-# création d'un byte envoyé au robot pour le contrôler
+# 0.3) création d'un byte envoyé au robot pour le contrôler
+
+#Utilisation de LEDBoard afin d'envoyer simultanément plusieurs signaux à différents GPIO du RPI, --> https://gpiozero.readthedocs.io/en/stable/recipes.html#ledboard
 #byte =         ["bit7",  "bit6",  "bit5",  "bit4",  "bit3",  "bit2",  "bit1",  "bit0"]
 byte = LEDBoard("GPIO18","GPIO23","GPIO24","GPIO25","GPIO12","GPIO16","GPIO20","GPIO21")
+
+
+# 0.4) Définition des menues
 
 def menu_principal(stdscr):
     stdscr.clear()
@@ -85,7 +91,8 @@ def menu_mode_manuel(stdscr):
     stdscr.refresh()
 
 
-#Sens de rotation inverse et normal
+# 0.5) Définitions des sens de rotations et des moteurs
+
 def sens_rotation_0() :
 	global sens_rotation
 	sens_rotation = 0
@@ -103,7 +110,8 @@ def sens_rotation_1() :
 	sleep(0.0005)
 
 
-#Rotation des moteurs en mode de test manuel :
+# 0.6) Définitions des moteurs
+
 def moteur_1() :
 	global moteurX, Codeur_moteur1, sens_rotation
 	moteurX = 1
@@ -177,20 +185,15 @@ def moteur_6() :
 		Codeur_moteur6 -= 1
 
 
-#signal RESET afin d'initialiser les phases du moteur --> 16#47 donc 2#01010111 ---> https://gpiozero.readthedocs.io/en/stable/recipes.html#ledboard
-byte.value = (0,1,0,0,0,1,1,1)
-print("initialisation en cours, byte = 47")
-sleep(0.2)
-byte.value = (0,0,0,0,0,0,0,0)
-print("initialisation en cours, byte = 0")
-sleep(0.2)
+# 0.7) Variables
 
-
-# Variables
+Menu = 1
+Menu_affiché = 0
 Mode_manuel = 0
 Test_moteurs = 0
-Menu = 1
-#Menu_affiché = 0
+Dance_robot = 0
+Choix_dance = 0
+
 Codeur_moteur1 = 0
 Codeur_moteur2 = 0
 Codeur_moteur3 = 0
@@ -200,80 +203,110 @@ Codeur_moteur6 = 0
 moteurX = 0
 sens_rotation = 0
 
+#def variables():
+#	Menu
+#	Menu_affiché
+#	Mode_manuel
+#	Test_moteurs
+#	Dance_robot
+#	Choix_dance
+#
+#	Codeur_moteur1
+#	Codeur_moteur2
+#	Codeur_moteur3
+#	Codeur_moteur4
+#	Codeur_moteur5
+#	Codeur_moteur6
+#	moteurX
+#	sens_rotation
 
 #===============================================================================================================================================================#
 #																																								#
 #																		Robot youpi	- Olivier DANIEL															#
-#																		Programme Principal																		#
+#																																								#
+#																		PROGRAMME PRINCIPAL																		#
 #																																								#
 #===============================================================================================================================================================#
 
+
+# Signal RESET afin d'initialiser les phases du moteur --> 16#47 donc 2#01000111 suivi de 16#0---> Voir "Documentation général.pdf"
+
+byte.value = (0,1,0,0,0,1,1,1)
+print("initialisation en cours, byte = 47")
+sleep(0.2)
+byte.value = (0,0,0,0,0,0,0,0)
+print("initialisation en cours, byte = 0")
+sleep(0.2)
+
 #===============================================================================#
 #																				#
 #								Robot youpi	- Olivier DANIEL					#
-#								Menu principal									#
+#																				#
+#								1) GESTION DES MENUES ET PROGRAMMES				#
 #																				#
 #===============================================================================#
 
-# MENU PRINCIPAL - Début du programme
-def main(stdscr):
-	curses.curs_set(0)					# cache le curseur
-	stdscr.nodelay(True)				# éntrées non blocantes
-	stdscr.timeout(100) 				# Timeout de 100ms pour getch()
 
-	#
-	global Mode_manuel, Test_moteurs, Menu, Codeur_moteur1, Codeur_moteur2, Codeur_moteur3, Codeur_moteur4, Codeur_moteur5, Codeur_moteur6, moteurX, sens_rotation		#, Menu_affiché
-	key = stdscr.getch()
+def main(stdscr): # ---> le fait que ça soit définit ralentis peut-être le programme ?
+	curses.curs_set(0)                  # cache le curseur
+	stdscr.nodelay(True)                # éntrées non blocantes
+	stdscr.timeout(100)                 # Timeout de 100ms pour getch()
+#	global variables
+	global Mode_manuel, Test_moteurs, Menu, Codeur_moteur1, Codeur_moteur2, Codeur_moteur3, Codeur_moteur4, Codeur_moteur5, Codeur_moteur6, moteurX, sens_rotation, Menu_affiché, Dance_robot, Choix_dance
 	stdscr.clear()
+	key = stdscr.getch()
 
 	while True : #key != 32 : # 32 en ASCII = Touche espace --> Arret d'URGENCE --> N'arettera pas le mouvement si un ordre est déja envoyé !! --> revoir ce système !
 
-#		if Menu == 1 and Menu_affiché == 0: (test de l'apparition unique du menu)
-#			menu_principal()
-#			Menu_affiché = 1
-
 		if Menu == 1:
-			menu_principal(stdscr)
-			stdscr.refresh()
-			key = stdscr.getch() 	# Capturer la touche appuyée
+			key = stdscr.getch()			# Capturer la touche appuyée
 			if key == ord('q') or key == ord('Q'):
 				break
+			elif Menu_affiché == 0:
+				menu_principal(stdscr)
+				stdscr.refresh()
+				Menu_affiché = 1
 
 			elif key == curses.KEY_F1:		# Mode manuel
 				Mode_manuel = 1
+				Menu_affiché = 0
 				Menu = 0
-
 			elif key == curses.KEY_F2:		# Choix du programme
+				Menu_affiché = 0
 				Menu = 2
 
 		elif Menu == 2:
-			stdscr.refresh()
-			menu_choix_programme(stdscr)
 			key = stdscr.getch()
-			if key == curses.KEY_F1:		#Test des moteurs
+			if key == 27:					# ESCAPE - retour au menu principal
+				Menu_affiché = 0
+				Menu = 1
+			elif Menu_affiché == 0:
+				menu_choix_programme(stdscr)
 				stdscr.refresh()
+				Menu_affiché = 1
+
+
+			elif key == curses.KEY_F1:		#Test des moteurs
 				Test_moteurs = 1
+				Menu_affiché = 0
 				Menu = 0
 			elif key == curses.KEY_F2:		# Dance des robot
-				stdscr.clear()
-				stdscr.addstr(0, 0,  "Pas encore programmé, retour au menu précédent...\n") #--> \n ?
-				stdscr.refresh()
-				sleep(2)
-			elif key == 27:					# ESCAPE
-				Menu = 1
-
+				Dance_robot = 1
+				Menu_affiché = 0
+				Menu = 0
 
 
 #===============================================================================#
 #																				#
 #								Robot youpi	- Olivier DANIEL					#
-#								Contrôle manuel									#
+#																				#
+#								2) CONTROLE MANUEL								#
 #																				#
 #===============================================================================#
 
-# Fonction principale pour le mode manuel
-		vitesse_manu = 200
+
 		while Mode_manuel == 1:               #--> remplacer par if pour que l'ATU fonctionne ?
+			vitesse_manu = 200
 			stdscr.refresh()
 			menu_mode_manuel(stdscr)
 			key = stdscr.getch()
@@ -285,16 +318,6 @@ def main(stdscr):
 			elif key == 10:	#Touche ENTER
 				globals()[f'Codeur_moteur{moteurX}'] = 0
 				print(moteurX)
-																	#Pour aider
-																	#Codeur_moteur1 = 0
-																	#Codeur_moteur2 = 0
-																	#Codeur_moteur3 = 0
-																	#Codeur_moteur4 = 0
-																	#Codeur_moteur5 = 0
-																	#Codeur_moteur6 = 0
-																	#
-																	#moteurX = 0
-																	#sens_rotation = 0
 
 			elif key == curses.KEY_F12:
 				sens_rotation_0()
@@ -328,22 +351,23 @@ def main(stdscr):
 				moteur_6()
 
 
-																													#curses.wrapper(main) --> voir à quoi ça sert
-
+					#curses.wrapper(main)
 
 
 #===============================================================================#
 #																				#
 #								Robot youpi	- Olivier DANIEL					#
-#								Test des moteurs								#
+#																				#
+#								3) TEST DES MOTEURS								#
 #																				#
 #===============================================================================#
+
 
 		while Test_moteurs == 1:
 			Nombre_de_pas_rotation_moteur = 400 # 200 pas = 1 tour moteur
 			stdscr.clear()
 			for i in range(1, 7):
-				Pos_txt = ((i-1)/(6/18)) #--> droite affine
+				Pos_txt = ((i-1)/(6/18)) # Droite affine pour la position des textes
 				Pos_txt = int(Pos_txt)
 
 				stdscr.addstr(Pos_txt, 0, "Test de rotation du moteur {} : ".format(i))
@@ -375,9 +399,30 @@ def main(stdscr):
 #===============================================================================#
 #																				#
 #								Robot youpi	- Olivier DANIEL					#
+#																				#
+#								Dance du robot									#
+#																				#
+#===============================================================================#
+
+
+		while Dance_robot == 1:
+			stdscr.clear()
+			stdscr.addstr(2, 0,  "Pas encore programmé, retour au menu précédent...\n") # --
+			stdscr.refresh()
+			sleep(2)
+			Menu = 2
+			Dance_robot = 0
+			Menu_affiché = 0
+			
+
+#===============================================================================#
+#																				#
+#								Robot youpi	- Olivier DANIEL					#
+#																				#
 #								Système d'arrêt d'urgence						#
 #																				#
 #===============================================================================#
+
 
 #	while key == 32:
 #		Mode_manuel = 0
@@ -391,14 +436,6 @@ def main(stdscr):
 #		stdscr.refresh()
 #		sleep(2)
 #		break
-
-
-#===============================================================================#
-#																				#
-#								Robot youpi	- Olivier DANIEL					#
-#								-------------------------						#
-#																				#
-#===============================================================================#
 
 
 if __name__ == "__main__":
